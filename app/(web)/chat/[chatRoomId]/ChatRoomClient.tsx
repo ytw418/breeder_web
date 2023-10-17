cbae371f160d68bea2771432485f5a41dde40cfe
neo-yoon <neo.yoon@ppoint.kr>
2023-10-17 18:24:06 +0900

    fix: 채팅룸 코드정리

--- app/(web)/chat/[chatRoomId]/ChatRoomClient.tsx
+++ app/(web)/chat/[chatRoomId]/ChatRoomClient.tsx
@@ -1,9 +1,9 @@
 "use client";
+
 import Layout from "@components/layout";
 import Message from "@components/message";
 import useSWR from "swr";
-import { useParams, useSearchParams } from "next/navigation";
-import { CarrotComment, TalkToSeller } from "@prisma/client";
+import { useParams } from "next/navigation";
 import { useForm } from "react-hook-form";
 import useMutation from "@libs/client/useMutation";
 import useUser from "@libs/client/useUser";
@@ -11,41 +11,28 @@ import { useEffect, useRef } from "react";
 import { ChatRoomResponseType } from "pages/api/chat/[chatRoomId]";
 import { produce } from "immer";
 
-interface MessageType {
-  message: string;
-  id: number;
-  user: {
-    avatar?: string;
-    id: number;
-  };
-}
-
-interface IForm {
+interface Form {
   message: string;
 }
 
 const ChatRoomClient = () => {
   const { user } = useUser();
+  console.log("클라이언트");
 
   const query = useParams();
-  const searchParams = useSearchParams();
-
-  // console.log("query :>> ", query);
 
   const { data, mutate } = useSWR<ChatRoomResponseType>(
     query?.chatRoomId && `/api/chat/${query?.chatRoomId}`
   );
 
-  console.log("12312312312312312312 :>> ", data);
-
   const [sendMessage, { loading, data: sendMessageData }] = useMutation(
     `/api/chat/${query?.chatRoomId}/message`
   );
-  const { register, handleSubmit, reset } = useForm<IForm>();
-  const onValid = (form: IForm) => {
+  const { register, handleSubmit, reset } = useForm<Form>();
+  const onValid = async (form: Form) => {
     if (form.message.length < 1) return;
     if (loading) return;
-    mutate(
+    await mutate(
       (prev) =>
         prev &&
         produce(prev, (drift) => {
@@ -62,17 +49,8 @@ const ChatRoomClient = () => {
         }),
       false
     );
+    scrollRef?.current?.scrollIntoView();
 
-    // ({
-    //   ...prev,
-    //   chatRoom: {
-    //     ...prev.chatRoom,
-    //     messages: [
-    //       ...prev?.chatRoom?.messages!,
-    //       { id: Date.now(), message: form.message, user: { ...user } },
-    //     ],
-    //   },
-    // } as ChatRoomResponseType)
     sendMessage({ data: form });
     reset();
   };
@@ -87,17 +65,8 @@ const ChatRoomClient = () => {
     scrollRef?.current?.scrollIntoView();
   }, []);
 
-  // const { data: findCarrotData } = useSWR<ISWRCarrotResponse>(
-  //   `/api/gotocarrot/${data?.findTalkToSellerUniq?.id}?sellerId=${data?.findTalkToSellerUniq?.createdSellerId}&buyerId=${data?.findTalkToSellerUniq?.createdBuyerId}&productId=${data?.findTalkToSellerUniq?.productId}`
-  // );
-
   return (
     <Layout canGoBack title="채팅" seoTitle="채팅">
-      {/* <CarrotDate
-        CarrotData={findCarrotData}
-        TTSData={data}
-        CarrotCommentData={carrotComment}
-      /> */}
       <div className="py-14 pb-16 px-4 space-y-4">
         {data?.chatRoom?.messages?.map((message, index) => (
           <Message
@@ -107,34 +76,7 @@ const ChatRoomClient = () => {
             reversed={user?.id === message.userId ? true : false}
           />
         ))}
-        {/* {data?.findTalkToSellerUniq?.messages.length !== 0 && (
-          <div
-            onClick={ClickBuy}
-            className="hover:cursor-pointer flex rounded-md relative max-w-md h-10 justify-center items-center  w-full mx-auto bg-orange-400"
-          >
-            {data?.findTalkToSellerUniq?.createdBuyerId === user?.id && (
-              <span>
-                {data?.findTalkToSellerUniq?.isbuy ? "예약 취소" : "구매 예약"}
-              </span>
-            )}
-            {data?.findTalkToSellerUniq?.createdSellerId === user?.id && (
-              <span>
-                {!data?.findTalkToSellerUniq?.isbuy &&
-                  data?.findTalkToSellerUniq?.issold &&
-                  "판매 취소"}
-                {data?.findTalkToSellerUniq?.isbuy &&
-                  data?.findTalkToSellerUniq?.issold &&
-                  "판매 취소"}
-                {data?.findTalkToSellerUniq?.isbuy &&
-                  !data?.findTalkToSellerUniq?.issold &&
-                  "판매 확정"}
-                {!data?.findTalkToSellerUniq?.isbuy &&
-                  !data?.findTalkToSellerUniq?.issold &&
-                  "판매 확정"}
-              </span>
-            )}
-          </div>
-        )} */}
+
         <form
           onSubmit={handleSubmit(onValid)}
           className="fixed pt-3 bg-white bottom-0 inset-x-0"
--- app/(web)/chat/[chatRoomId]/page.tsx
+++ app/(web)/chat/[chatRoomId]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ChatRoomClient from "./ChatRoomClient";
 
 const page = () => {
+  console.log("서버컴포넌트");
   return <ChatRoomClient />;
 };
 
