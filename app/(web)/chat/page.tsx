6a4f5e5d69632210aed8f05eaebfd21d277d895a
neo-yoon <neo.yoon@ppoint.kr>
2023-10-18 15:04:12 +0900

    chore:1

--- app/(web)/chat/page.tsx
+++ app/(web)/chat/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ChatsClient from "../chat/ChatClient";
 
-const page = () => {
+const Page = () => {
   return <ChatsClient />;
 };
 
-export default page;
+export default Page;
