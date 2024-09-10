d8ab8741499bd9ecfbd146065cf5e26de17fd29a
neo-yoon <neo.yoon@ppoint.kr>
2024-09-10 13:05:46 +0900

    feat: shadcn 세팅 [New York,Neutral,yes]

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,40 +2,85 @@
 
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        Black: "#191919",
-        White: "#FFFFFF",
-        Primary: "#f97316",
-        Purple: "#5F50F0",
-        Red: "#F66570",
-        Modal: "#000000",
-        Gray: {
-          100: "#FAFAFA",
-          200: "#F6F6F6",
-          300: "#E5E5E5",
-          400: "#B2B2B2",
-          500: "#7F7F7F",
-          600: "#4C4C4C",
-        },
-      },
-      fontFamily: {
-        pretendard: ["Pretendard Variable", ...defaultTheme.fontFamily.sans],
-        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
-      },
-    },
+  	extend: {
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		colors: {
+  			Black: '#191919',
+  			White: '#FFFFFF',
+  			Primary: '#f97316',
+  			Purple: '#5F50F0',
+  			Red: '#F66570',
+  			Modal: '#000000',
+  			Gray: {
+  				'100': '#FAFAFA',
+  				'200': '#F6F6F6',
+  				'300': '#E5E5E5',
+  				'400': '#B2B2B2',
+  				'500': '#7F7F7F',
+  				'600': '#4C4C4C'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			pretendard: ["Pretendard Variable", ...defaultTheme.fontFamily.sans],
+  			poppins: ["Poppins", ...defaultTheme.fontFamily.sans]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
 
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [require("tailwind-scrollbar-hide"), require("tailwindcss-animate")],
 };
