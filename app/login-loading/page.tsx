a817feeb84266b11f513fb9ba7b644a82f2bd5e4
ytw418 <ytw418@gmail.com>
2025-04-20 22:12:15 +0900

    feat: seo 구글 추가

--- app/login-loading/page.tsx
+++ app/login-loading/page.tsx
@@ -3,8 +3,8 @@ import KakaoLogin from "@components/auth/KakaoLogin";
 import { Suspense } from "react";
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "title",
-    description: "description",
+    title: "브리더 로그인 페이지",
+    description: "브리더 로그인 페이지입니다.",
   };
 }
 
