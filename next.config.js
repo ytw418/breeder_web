31f05ae978d3bbbb32fe8791da80aae9a322b84e
ytw418 <ytw418@gmail.com>
2025-06-01 17:12:48 +0900

    fix: 모든 외부 이미지 허용

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "**" }],
+    remotePatterns: [{ hostname: "**" }],
   },
 };
 
