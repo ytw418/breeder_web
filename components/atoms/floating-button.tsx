849b5c6946504bc51e73c2c0b06f6517770606bd
ytw418 <ytw418@gmail.com>
2026-02-09 14:09:40 +0900

    fix: add z-index to floating button for improved visibility

--- components/atoms/floating-button.tsx
+++ components/atoms/floating-button.tsx
@@ -10,7 +10,7 @@ export default function FloatingButton({ children, href }: FloatingButton) {
   return (
     <Link
       href={href}
-      className="fixed hover:bg-orange-500 border-0 aspect-square border-transparent transition-colors cursor-pointer  bottom-24 right-5 shadow-xl bg-orange-400 rounded-full w-14 flex items-center justify-center text-white"
+      className="fixed z-50 hover:bg-orange-500 border-0 aspect-square border-transparent transition-colors cursor-pointer bottom-24 right-5 shadow-xl bg-orange-400 rounded-full w-14 flex items-center justify-center text-white"
     >
       {children}
     </Link>
