f377f1b071c404f3ea15a02887dac265aa0cc001
윤성준 <ytw418@gmail.com>
2026-02-19 23:30:36 +0900

    로그인 관심 분류 선택 도입안 및 시점 검토 추가

--- pages/api/auctions/index.ts
+++ pages/api/auctions/index.ts
@@ -13,6 +13,7 @@ import {
 import { settleExpiredAuctions } from "@libs/server/auctionSettlement";
 import { getCategoryFilterValues } from "@libs/categoryTaxonomy";
 import { normalizeOptionalText, normalizeOptionalUrl } from "@libs/shared/normalize";
+import { getCategoryFilterValues } from "@libs/categoryTaxonomy";
 
 /** 경매 목록 응답 타입 */
 export interface AuctionWithUser extends Auction {
