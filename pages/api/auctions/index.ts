ac008f2c45182936402c2703ad34b4ebe6094037
ytw418 <ytw418@gmail.com>
2026-02-23 13:23:55 +0900

    리베이스 후 중복 선언 정리

--- pages/api/auctions/index.ts
+++ pages/api/auctions/index.ts
@@ -13,7 +13,6 @@ import {
 import { settleExpiredAuctions } from "@libs/server/auctionSettlement";
 import { normalizeOptionalText, normalizeOptionalUrl } from "@libs/shared/normalize";
 import { getCategoryFilterValues } from "@libs/categoryTaxonomy";
-import { normalizeOptionalText, normalizeOptionalUrl } from "@libs/shared/normalize";
 
 /** 경매 목록 응답 타입 */
 export interface AuctionWithUser extends Auction {
